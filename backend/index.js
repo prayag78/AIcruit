bd9dfec5f11fe9ea346a38fd58d4704b13cce550
prayag78 <dasprayag1234@gmail.com>
2025-04-06 16:22:41 +0530

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
-// /api/index.js
-
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import userRouter from './routes/userRoutes.js';
+import recruiterRouter from './routes/recruiterRoutes.js';
+import connectCloudinary from './config/cloudinary.js'
 import connectDB from './config/mongodb.js';
-import userRouter from '../routes/userRoutes.js';
-import recruiterRouter from '../routes/recruiterRoutes.js';
-import connectCloudinary from '../config/cloudinary.js';
 
+//app config
 const app = express();
+const port = process.env.PORT || 8000;
 
 connectDB();
 connectCloudinary();
@@ -16,12 +16,19 @@ connectCloudinary();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/user',userRouter)
+app.use('/api/recruiter',recruiterRouter)
+
+
 app.use('/api/user', userRouter);
 app.use('/api/recruiter', recruiterRouter);
 
 app.get('/', (req, res) => {
+    res.send(`Server started at port ${port}`);
   res.send('Serverless backend running on Vercel 🔥');
 });
 
+app.listen(port, () => console.log("Server Started at",port));
 // ✅ DO NOT use app.listen()
 export default app; // Important for Vercel
+
